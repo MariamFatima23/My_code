fe63f95ee351ccdd9b5ded694f36698780a45c3b
MariamFatima23 <mariamfatii78@gmail.com>
2024-08-24 21:34:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// controllers/userController.js
+export const createUser = async (req, res) => { /* ... */ };
+export const getAllUsers = async (req, res) => { /* ... */ };
+export const getUserById = async (req, res) => { /* ... */ };
+export const updateUser = async (req, res) => { /* ... */ };
+export const deleteUser = async (req, res) => { /* ... */ };
+// routes/users.js
+const PORT = process.env.PORT || 5000;
+
+
+
+// Your route handlers
+console.log('Hello, world!');
